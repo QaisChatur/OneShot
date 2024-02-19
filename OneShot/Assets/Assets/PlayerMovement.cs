b8255462c17c436282bc8781a47b4dfb201710db
9iero <piero2@rogers.com>
2024-02-18 21:07:13 -0500

    Jump + Sprite Update
    still a bug where the sprite tips over idk why

--- OneShot/Assets/Assets/PlayerMovement.cs
+++ OneShot/Assets/Assets/PlayerMovement.cs
@@ -9,8 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
     private float jump = 300;
 
-    private Rigidbody2D rb;
+    public bool isJumping;
 
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +24,38 @@ public class PlayerMovement : MonoBehaviour
     {
         move = Input.GetAxis("Horizontal");
 
+        // Flip the character sprite if moving left
+        if (move < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Flip horizontally
+        }
+        else if (move > 0) // If moving right
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Reset scale
+        }
+
         rb.velocity = new Vector2(speed * move, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W)) && isJumping == false) // Jump with W or Space, only if grounded
         {
             rb.AddForce(new Vector2 (rb.velocity.x, jump));
+            Debug.Log("jump");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
         }
     }
 }
