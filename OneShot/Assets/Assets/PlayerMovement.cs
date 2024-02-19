c26be9e8b896a75d52e4e67d6320a3fc459d54d2
9iero <piero2@rogers.com>
2024-02-18 20:29:16 -0500

    Added PlayerMovement
    jump with spacebar or w, a and d movement keys

--- OneShot/Assets/Assets/PlayerMovement.cs
+++ OneShot/Assets/Assets/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private float move;
+
+    private float jump = 300;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2 (rb.velocity.x, jump));
+        }
     }
 }
